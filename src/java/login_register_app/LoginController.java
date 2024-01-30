6b421233d4a150b73d63002b6e1de5a8e07681d8
Comandao <comandaoashleyken@gmail.com>
2024-01-31 00:42:14 +0800

    add scene-switching functionality to the login/signup buttons

--- src/java/login_register_app/LoginController.java
+++ src/java/login_register_app/LoginController.java
@@ -1,14 +1,47 @@
 package login_register_app;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class LoginController {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    /**
+     * SWITCHING TO LOGIN/SIGNUP SCENE
+     * Handles the login/signup button on the left side of the scenes.
+     * On click, it switches to the other scene.
+     */
+    @FXML Button switchToSignUp;
+    @FXML Button switchToLogin;
     @FXML
-    private Label welcomeText;
+    public void switchToLogin(ActionEvent actionEvent) throws IOException {
+        this.root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("fxml/Login.fxml")));
+        this.stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        this.scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.show();
+    }
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void switchToSignUp(ActionEvent actionEvent) throws IOException {
+        this.root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("fxml/SignUp.fxml")));
+        this.stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        this.scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.show();
     }
 }
